40f6ae652ae0d5766b6b7129644c3bf4bd09a6a7
Eszter Egyud <eszter.egyud@telus.com>
2022-11-03 14:55:10 -0600

    calculating odd numbers

---
+++ odd.js
@@ -0,0 +1,18 @@
+
+
+const isOdd = function (num) {
+  return num % 2 === 1;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+
+
+/*
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+console.log(isEven(10));
+console.log(isEven(11));
+*/
